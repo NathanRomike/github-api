e6d61776dd203d0d817db5fb046aea14b6c295f9
NathanRomike <nathanromike@gmail.com>
2016-02-19 10:28:13 -0800

    Create for loop to parse response json and display repo names to page.

--- js/browser.js
+++ js/browser.js
@@ -1,7 +1,11 @@
-var apiKey = "0461ef28c848aa6ce3fc1a3c693ac17b2afd708f"
-
 $(document).ready(function(){
-  $.get('https://api.github.com/users/NathanRomike/repos?&apiid=' + apiKey, function(data) {
-    console.log(data);
+  $("#input-form").submit(function(event) {
+    event.preventDefault();
+    var userName = $("#userName").val();
+    $.get('https://api.github.com/users/' + userName + '/repos?&apiid=0461ef28c848aa6ce3fc1a3c693ac17b2afd708f', function(response) {
+      for (var i = 0; i < response.length; i++) {
+        $('.showRepos').append("<a href=" + "'" + response[i].html_url + "'" + ">" + response[i].name + "</a>" + "<br>");
+      }
+    });
   });
 });
