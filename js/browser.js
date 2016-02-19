1fbf49fcdc668e4d4e9a6b5b05ea3fafca2cc314
NathanRomike <nathanromike@gmail.com>
2016-02-19 11:22:33 -0800

    Add error message if user has no repos.

--- js/browser.js
+++ js/browser.js
@@ -4,8 +4,13 @@ $(document).ready(function(){
     $('.showRepos').empty();
     var userName = $("#userName").val();
     $.get('https://api.github.com/users/' + userName + '/repos?&apiid=0461ef28c848aa6ce3fc1a3c693ac17b2afd708f', function(response) {
-      for (var i = 0; i < response.length; i++) {
-        $('.showRepos').append("<a href=" + "'" + response[i].html_url + "'" + "target='_blank'" + ">" + response[i].name + "</a>" + "<br>");
+    $('.showRepos').html("<h2>" + userName + "</h2>");
+      if (response.length === 0) {
+        $('.showRepos').text("No repositories found for this user.")
+      } else {
+        for (var i = 0; i < response.length; i++) {
+          $('.showRepos').append("<a href=" + "'" + response[i].html_url + "'" + "target='_blank'" + ">" + response[i].name + "</a>" + "<br>");
+        }
       }
     });
   });
