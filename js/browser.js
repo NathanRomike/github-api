bab2368572d54396c6ba1065a8e0a80ca9e4ec3f
NathanRomike <nathanromike@gmail.com>
2016-02-19 10:48:34 -0800

    Add empty method to clear div between searches.

--- js/browser.js
+++ js/browser.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   $("#input-form").submit(function(event) {
     event.preventDefault();
+    $('.showRepos').empty();
     var userName = $("#userName").val();
     $.get('https://api.github.com/users/' + userName + '/repos?&apiid=0461ef28c848aa6ce3fc1a3c693ac17b2afd708f', function(response) {
       for (var i = 0; i < response.length; i++) {
